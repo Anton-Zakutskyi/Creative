57d6e68400a3b111c5482fa2e5c31fd6cd3f2e07
Anton-Zakutskyi <anton888000@gmail.com>
2021-12-18 23:55:42 +0200

    Competed PopUp for "Оставить заявку"

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            0:{
+                items:1
+            },
+
+            1000:{
+                items:1
+            }
+        }
+    });
+});
+
+document.getElementById('btn_click').addEventListener("click", function() {
+    document.querySelector('.bg-modal').style.display = "flex";
+});
+
+document.querySelector('.close').addEventListener("click", function() {
+    document.querySelector('.bg-modal').style.display = "none";
 });
